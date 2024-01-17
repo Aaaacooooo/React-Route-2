095f7c22484a4131ba3e7a1abef650b8908125b4
Aaaacooooo <acoray06@gmail.com>
2024-01-17 19:12:41 +0000

    Navbar creado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar  from "./Navbar.jsx";
+function App() {
+
+  return (
+    <>
+      <div>Esto es un div</div>
+    <Navbar>Este es un navbar</Navbar>
+    </>
+  )
+}
+
+export default App
