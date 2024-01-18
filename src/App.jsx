cfe2c6c8e47aaa7651662632603c69991f66a3fb
Aaaacooooo <acoray06@gmail.com>
2024-01-18 12:59:59 +0000

    /blog/:id - Budquedas personalizadas

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Contacto from './pages/Contacto.jsx';
 import Blog from './pages/Blog.jsx';
 import NotFound from './pages/NotFound.jsx';
 import Layout from './layouts/Layout.jsx';
+import BlogCharacter from './pages/BlogCharacter.jsx';
 
 function App() {
   return (
@@ -13,11 +14,12 @@ function App() {
       <Navbar />
       <h1>App</h1>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/contacto" element={<Contacto />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="*" element={<NotFound />} />
+        <Route element={<Layout />} path="/">
+          <Route element={<Inicio />} path="/" />
+          <Route element={<Contacto />} path="/contacto" />
+          <Route element={<Blog />} path="/blog" />
+          <Route element={<BlogCharacter />} path="/blog/:id" />
+          <Route element={<NotFound />} path="*" />
         </Route>
       </Routes>
     </>
---
+++ src/pages/BlogCharacter.jsx
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import useFetch from './../componets/useFetch';
+
+const BlogCharacter = () => {
+    const params = useParams();
+    const url = useFetch(`https://rickandmortyapi.com/api/character/${params.id}`);
+    const { data, error, loading } = useFetch(
+        url
+    );
+
+    if (loading) return (<h1>Buscando el Morty adecuado...</h1>)
+    if (error) return (<h1>La pistola de portales no funciona...</h1>)
+
+    // console.log(data);
+    return (
+        <div className='card'>
+            <img src={data.image} className='card-img-top' alt="..." />
+            <div className='card-body'>
+                <h5 className='card-title'>{data.name}</h5>
+                <p className='card-text'>{data.species}</p>
+            </div>
+        </div>
+    );
+};
+
+export default BlogCharacter;
