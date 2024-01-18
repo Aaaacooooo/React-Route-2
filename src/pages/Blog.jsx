204f73a82ea4242b65c86f434deccc3171784709
Aaaacooooo <acoray06@gmail.com>
2024-01-18 17:14:01 +0000

    Busqueda por nombre

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import useFetch from './../componets/useFetch';
 import { useSearchParams } from 'react-router-dom';
 
@@ -6,11 +7,33 @@ const Blog = () => {
     const { data, error, loading } = useFetch('https://rickandmortyapi.com/api/character');
     //con serchParams.get(nombre accedemos al valor de la url que queremos
     //con el setSearchParams establecemos el valor
-    let [searchParams, setSerchParams] = useSearchParams();
+    // let [searchParams, setSerchParams] = useSearchParams();
+    // const handleChange = (e) => {
+    //     setSerchParams({ [e.target.name]: e.target.value })
+    // }
+
+    const [searchParams] = useSearchParams();
+    const filter = searchParams.get('filter') || '';
+
+    const [searchInput, setSearchInput] = useState(filter);
+
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
-        setSerchParams({ [e.target.name]: e.target.value })
-    }
+        setSearchInput(e.target.value);
+    };
+
+    // Manejador de la acción de búsqueda
+    const handleSearch = () => {
+        // Actualiza el parámetro 'filter' en la URL y navega
+        navigate(`/blog?filter=${encodeURIComponent(searchInput)}`);
+    };
+
+    const filteredCharacters = data
+        ? data.results.filter((character) =>
+            character.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        : [];
 
 
     if (loading) return (<h1>Buscando el Morty adecuado...</h1>)
@@ -21,7 +44,19 @@ const Blog = () => {
         <div>
             <h2>Blog - Elige tu personaje favorito</h2>
 
-            <input
+            <input type="text" value={searchInput} onChange={handleChange} />
+            <button onClick={handleSearch}>Buscar</button>
+
+            {filteredCharacters.map(item => (
+                <div key={item.id}>
+                    <Link to={`/blog/${item.id}`}>
+                        <h3>{item.name}</h3>
+                    </Link>
+                </div>
+            ))}
+
+
+            {/* <input
                 type="text"
                 name='filter'
                 onChange={handleChange}
@@ -40,7 +75,7 @@ const Blog = () => {
                                 {item.name}</Link>)
                     })
                 }
-            </ul>
+            </ul> */}
         </div>
     );
 };
