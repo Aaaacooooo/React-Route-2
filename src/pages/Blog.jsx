b4d5b55f22c2eada1d667830d41e6355beeb2ff3
Aaaacooooo <acoray06@gmail.com>
2024-01-18 13:43:25 +0000

    Parte 5 - useSearchParams

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,17 @@
 import { Link } from 'react-router-dom';
 import useFetch from './../componets/useFetch';
+import { useSearchParams } from 'react-router-dom';
 
 const Blog = () => {
     const { data, error, loading } = useFetch('https://rickandmortyapi.com/api/character');
+    //con serchParams.get(nombre accedemos al valor de la url que queremos
+    //con el setSearchParams establecemos el valor
+    let [searchParams, setSerchParams] = useSearchParams();
+
+    const handleChange = (e) => {
+        setSerchParams({ [e.target.name]: e.target.value })
+    }
+
 
     if (loading) return (<h1>Buscando el Morty adecuado...</h1>)
     if (error) return (<h1>La pistola de portales no funciona...</h1>)
@@ -11,6 +20,16 @@ const Blog = () => {
     return (
         <div>
             <h2>Blog - Elige tu personaje favorito</h2>
+
+            <input
+                type="text"
+                name='filter'
+                onChange={handleChange}
+                className='form-control my-3'
+                alt='Buscador'
+                value={searchParams.get('filter') || ''}
+            />
+
             <ul className='list-group'>
                 {
                     data.results.map(item => {
